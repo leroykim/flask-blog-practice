f7bc5e29bd875a5db19eef661db38dc7906472e8
Dae-young Leroy Kim <leroy.kim@umbc.edu>
2020-03-15 23:03:48 -0400

    Add `Jinja` feature

--- flaskblog.py
+++ flaskblog.py
@@ -5,22 +5,36 @@
 # Auto reload
 # > set FLASK_DEBUG=1
 
-from flask import Flask, escape, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Leroy Kim',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 15, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 16, 2020'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    # http://localhost:5000/?name=leroy
-    name = request.args.get("name", "World")
-    return f'<h1>Hello, {escape(name)}?</h1>'
+    # Name of variable does not matter
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return f'<h1>About</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
