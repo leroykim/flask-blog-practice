62039d136a5008a0937b736f152a6f111319708f
Dae-young Leroy Kim <leroy.kim@umbc.edu>
2020-03-16 22:24:06 -0400

    Add register page
    WIP error message from wrong input (32:33)

--- flaskblog.py
+++ flaskblog.py
@@ -5,9 +5,12 @@
 # Auto reload
 # > set FLASK_DEBUG=1
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationFrom, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '33a6d89745efb0d7e4f219430aa6672d'
 
 posts = [
     {
@@ -35,6 +38,21 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationFrom()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
